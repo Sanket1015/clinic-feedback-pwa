07c48553cdb226ad9959c66ccff184879ba5599a
Sanket1015 <134353371+Sanket1015@users.noreply.github.com>
2025-12-24 20:15:46 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'clinic-feedback-v10'; // v10
+const CACHE_NAME = 'clinic-feedback-v13'; // Bumped to v13
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -6,7 +6,7 @@ const ASSETS_TO_CACHE = [
   'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
   'https://cdn.jsdelivr.net/npm/chart.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/qrious/4.0.2/qrious.min.js' // NEW: Offline QR Library
+  'https://cdnjs.cloudflare.com/ajax/libs/qrious/4.0.2/qrious.min.js'
 ];
 
 self.addEventListener('install', (e) => {
